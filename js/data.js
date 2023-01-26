304b572b0462c4c11bd2ec50c9bb29ba92c00fa9
Caio César Silveira Chaves Silva <ext.caio.silva@burgerking.com.br>
2023-01-26 15:04:34 -0300

    Added more content

--- js/data.js
+++ js/data.js
@@ -1,5 +1,47 @@
 DATA = [
     {
+        name: "A Garota do Lago",
+        description: "Uma estudante de direito foi brutalmente assassinada em uma dessas casas numa pequena cidade entre montanhas.",
+        image: "images/agarotadolago.png",
+        url: "",
+        author: "Charlie Donlea",
+    },
+    {
+        name: "Todas as suas (im)perfeições",
+        description: "Uma história de amor perfeita é suficiente para manter vivo o casamento entre duas pessoas imperfeitas?",
+        image: "images/todasassuasimperfeicoes.png",
+        url: "",
+        author: "Colleen Hoover",
+    },
+    {
+        name: "Os Sete Maridos de Evelyn Hugo",
+        description: "Uma narrativa inesquecível sobre os sacrifícios que fazemos por amor, o perigo dos segredos e o preço da fama.",
+        image: "images/ossetemaridosdeevelynhugo.png",
+        url: "",
+        author: "Taylor Jenkins Reid",
+    },
+    {
+        name: "O Homem Mais Rico da Babilônia",
+        description: "É um clássico sobre como multiplicar riqueza e solucionar problemas financeiros.",
+        image: "images/ohomemmaisricodababilonia.png",
+        url: "",
+        author: "George S. Clason",
+    },
+    {
+        name: "É Assim que Acaba",
+        description: "Um romance sobre as escolhas corretas nas situações mais difíceis.",
+        image: "images/eassimqueacaba.png",
+        url: "",
+        author: "Colleen Hoover",
+    },
+    {
+        name: "É Assim que Começa",
+        description: "Lily e Atlas estão de volta na aguardada sequência de É assim que acaba.",
+        image: "images/eassimquecomeca.png",
+        url: "https://sun.eduzz.com/1768558?a=95318303",
+        author: "Colleen Hoover",
+    },
+    {
         name: "Os Segredos da Mente Milionária",
         description: "Aprenda a enriquecer mudando seus conceitos sobre dinheiro.",
         image: "images/ossegredosdamentemilionaria.png",
